ab9cd4da9dfdb57a8e3aef4732d41cef884970de
João Pedro <joaopedroboaretto@gmail.com>
2025-10-21 21:20:52 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+    Algoritmos:
+    - BubbleSort
+    - SelectionSort
+    - InsertionSort
+    - ShellSort
+    - QuickSort (mediana de 3)
+    - HeapSort
+    - MergeSort
+    - Contagem dos menores
+    - RadixSort
+*/
+
+int main() {
+    return 0;
+}
